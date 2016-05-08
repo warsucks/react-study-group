b6adf6440a25401d28683dc6f0b1dc4f7b0220c2
Kathy Lu <kaffy.lu@gmail.com>
2016-05-08 14:02:06 -0400

    Restructured files, added gulp build system, fleshed out game board

---
+++ src/react-src/components/square.js
@@ -0,0 +1,14 @@
+let React = require('react');
+
+let Square = React.createClass({
+
+	render: function() {
+		return (
+			<div className="square"> Square
+			</div>
+		)
+	}
+});
+
+module.exports = Square;
+
