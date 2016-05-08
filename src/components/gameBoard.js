f1759fba39a9735164d5f9031e937192a4c87522
Kathy Lu <kaffy.lu@gmail.com>
2016-05-08 17:19:19 -0400

    Added to gameboard

--- src/components/gameBoard.js
+++ src/components/gameBoard.js
@@ -3,21 +3,24 @@ let Square = require('./square')
 
 let GameBoard = React.createClass({
 	render: function() {
-
-		// let gridModel = [];
-		// let rowOfSquares = []
-
-		// // for(let r = 0; r < this.props.numRows; r++){
-		// 	let row = []
-		// 	let r = 0
-		// 	for(let c = 0; c< this.props.numCols; c++){
-		// 		row.push(<Square row={r} col={c} />)
-		// 	}
-		// // }
-
-		//return row;
 		return (
-			<Square />
+		<div className="game-board">
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+		</div>
 		)
 	}
 })
