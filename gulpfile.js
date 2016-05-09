6551f93f502993986eb3de82af27b368df77d71e
Dave Addison <djaddison@gmail.com>
2016-05-08 23:46:48 -0400

    Removed path resolution from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-var path        = require('path');
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var browserSync = require('browser-sync');
@@ -6,40 +5,29 @@ var browserify  = require('browserify')
 var babelify    = require('babelify')
 var source      = require('vinyl-source-stream');
 
-options = {
-   basedir: __dirname,
-   dst: path.join(__dirname, 'public/'),
-   style: path.join(__dirname, 'src/app.scss'),
-   js: path.join(__dirname, 'src/app.js'),
-   bundle: 'app.bundle.js',
-   index: path.join(__dirname, 'src/index.html'),
-   watch: path.join(__dirname, 'src/**/*'),
-   reloadWatch: path.join(__dirname, 'public/**/**')
-}
-
 // Style
 gulp.task('style', function() {
-   return gulp.src(options.style)
+   return gulp.src( 'src/app.scss' )
       .pipe(sass({
          style: 'expanded',
          noCache: true
       }))
-      .pipe(gulp.dest(options.dst));
+      .pipe(gulp.dest( 'public/' ));
 });
 
 //JS: Make a browserify bundle, with source files transpiled
 gulp.task('js', function () {
-   browserify(options.js)
+   browserify('src/app.js')
       .transform(babelify, {presets: ['es2015', 'react']})
       .bundle()
-      .pipe(source(options.bundle))
-      .pipe(gulp.dest(options.dst));
+      .pipe(source('app.bundle.js'))
+      .pipe(gulp.dest('public/'));
 });
 
 // Copy index.html
 gulp.task('copy', function () {
-   return gulp.src(options.index)
-      .pipe(gulp.dest(options.dst));
+   return gulp.src('src/index.html')
+      .pipe(gulp.dest('public/'));
 });
 
 // Static Server with Reload
@@ -47,7 +35,7 @@ gulp.task('server', function() {
    return browserSync.init(null, {
       open: true,
       server: {
-         baseDir: options.dst
+         baseDir: './public'
       },
       watchOptions: {
          debounceDelay: 300
@@ -57,12 +45,12 @@ gulp.task('server', function() {
 
 // Watch for changes
 gulp.task('watch', function() {
-   gulp.watch(options.watch + '.scss', ['style']);
-   gulp.watch(options.watch + '.js', ['js']);
-   gulp.watch(options.watch + '.jsx', ['js']);
-   gulp.watch(options.index, ['copy']);
+   gulp.watch('src/**/*.scss', ['style']);
+   gulp.watch('src/**/*.js', ['js']);
+   gulp.watch('src/**/*.jsx', ['js']);
+   gulp.watch('src/index.html', ['copy']);
 
-   return gulp.watch(options.reloadWatch, function(file) {
+   return gulp.watch('public/**/**', function(file) {
       if (file.type === 'changed') {
          return browserSync.reload(file.path);
       }
